2433b3443a94902224df9cb51d350a7346230aef
M Subhan Naeem <sub.metu@gmail.com>
2019-08-06 17:59:50 +0300

    adds basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
-import React from 'react';
-import './App.css';
-import { AuthProvider } from './auth/AuthProvider';
-import './components';
-import { Home } from './components/Home';
-import { AppContextProvider } from './core/AppContextProvider';
-import './shared';
-import { useTheme } from './themes';
+import React from "react";
+import { createBrowserHistory } from 'history'
+import "./App.css";
+import "./shared";
+import "./components";
+import { Home } from "./components/Home";
+import { AuthProvider } from "./auth/AuthProvider";
+import { AppContextProvider } from "./core/AppContextProvider";
+import { useTheme } from "./themes";
+import { Router, Route } from "react-router";
 
+const history = createBrowserHistory();
 function App() {
   const [currentTheme] = useTheme();
 
   return (
     <div className="App">
-      <AppContextProvider theme={currentTheme}>
-        <AuthProvider>
-          <Home />
-        </AuthProvider>
-      </AppContextProvider>
+      <Router history={history}>
+        <AppContextProvider theme={currentTheme}>
+          <AuthProvider>
+            <Route path="/" component={Home} />
+          </AuthProvider>
+        </AppContextProvider>
+      </Router>
     </div>
   );
 }
